5d23dfb3747cdde3870ad818bca5c304872841e3
surkarun <surkarun@amazon.com>
2020-05-15 15:59:25 +0530

    updated doRegister

--- api/doRegister.php
+++ api/doRegister.php
@@ -3,9 +3,11 @@
 	$name=$_REQUEST['name'];
 	$email=$_REQUEST['email'];
 	$pass=$_REQUEST['pass'];
-	
+	$type="user";
+	if(isset($_REQUEST['type']))
+		$type = $_REQUEST['type'];
 	require('db.php');
-	$sql="INSERT INTO user( username, email, pass, userType, submit) VALUES ('$name','$email','$pass','user',NOW())";
+	$sql="INSERT INTO user( username, email, pass, userType, submit) VALUES ('$name','$email','$pass','".$type."',NOW())";
 	$result = $conn->query($sql);
 	
 echo $result;
